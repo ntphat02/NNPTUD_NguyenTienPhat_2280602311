26babb9c324d4f73584fbfee6f9875dd346302a2
Phat <02ntphat@gmail.com>
2025-09-25 11:28:08 +0700

    update

--- schemas/product.js
+++ schemas/product.js
@@ -1,29 +1,32 @@
-let mongoose = require('mongoose');
+let mongoose = require("mongoose");
 
-let schema = new mongoose.Schema({
-    name:{
-        type:String,
-        required: [true,"thang nay khong duoc de trong"],
-        unique:true
+let schema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "thang nay khong duoc de trong"],
+      unique: true,
     },
-    price:{
-        type:Number,
-        default:1
+    price: {
+      type: Number,
+      default: 1,
     },
-    description:{
-        type:String,
-        default: "good product"
+    description: {
+      type: String,
+      default: "good product",
     },
-    category:{
-        type:String,
-        required: true
-    }
-    ,
-    isDelete:{
-        type:Boolean,
-        default: false
-    }
-},{
-    timestamps:true
-})
-module.exports = new mongoose.model('product',schema)
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "category",
+      required: true,
+    },
+    isDelete: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+module.exports = new mongoose.model("product", schema);
